2c0b2a27e6a4657e7244fddac1d3df42efdb6bef
Pallavi Mirage <pallavi.vikas.mirage@accenture.com>
2021-07-08 17:47:45 +0530

    Created Customer API Service

---
+++ Customer/Controllers/CustomerController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Customer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CustomerController : ControllerBase
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Pallavi", "Irshad", "Bhavana", "Madhura", "Nitish" };
+          
+        }
+
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return $"You are passing Id is - {id}";
+        }
+    }
+}
